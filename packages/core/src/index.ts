8d1b621842e14142ef6443e31f45fc772fddf366
vigovlugt <vigovlugt@gmail.com>
2024-05-09 15:11:19 +0200

    Finish migration to monorepo

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./mutation";
+export * from "./pull";
+export * from "./push";
+export * from "./relic-definition-builder";
+export * from "./relic-definition-mutation";
+export * from "./relic-definition";
+export * from "./relic-mutation";
