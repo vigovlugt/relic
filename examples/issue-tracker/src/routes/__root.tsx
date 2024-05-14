23da32c24dd4ba4a047ef2fe5b26edb4f0c5cc2b
vigovlugt <vigovlugt@gmail.com>
2024-05-14 09:18:14 +0200

    Change pending mutations

--- examples/issue-tracker/src/routes/__root.tsx
+++ examples/issue-tracker/src/routes/__root.tsx
@@ -13,8 +13,34 @@ function Root() {
     const { data: pending } = useSuspenseQuery(relic.pendingMutations());
     return (
         <div>
-            <div>{pending.length ? "Syncing..." : "Synced"}</div>
             <Outlet />
+            <details
+                style={{
+                    position: "absolute",
+                    bottom: "8px",
+                    left: "8px",
+                }}
+            >
+                <summary>Mutation queue</summary>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Id</td>
+                            <td>Name</td>
+                            <td>Input</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {pending?.map((mutation) => (
+                            <tr key={mutation.id}>
+                                <td>{mutation.id}</td>
+                                <td>{mutation.name}</td>
+                                <td>{JSON.stringify(mutation.input)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </details>
         </div>
     );
 }
